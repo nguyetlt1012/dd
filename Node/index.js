0c356a630165c70ddf48687cfe427bb4b52ef458
nguyetlt1012 <lenguyetds@gmail.com>
2021-12-22 17:05:43 +0700

    connect db

--- Node/index.js
+++ Node/index.js
@@ -1,1 +1,17 @@
-console.log("hello mina")
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dontenv=require("dotenv")
+
+dontenv.config();
+
+mongoose
+  .connect(
+    process.env.MONGO_URL
+  )
+  .then(() => console.log("DB connection sucessfull!"))
+  .catch((erro) => console.log(erro));
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Backend server is running");
+});
